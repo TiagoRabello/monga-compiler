d7bd2c5ca3e2a33a61519475d8f414e62817f485
Tiago Rabello <tiago.gomes_mtg@hotmail.com>
2015-05-18 11:36:41 -0300

    Rollback scanner and parser tests output changes because it breaks Makefile automated test targets.

--- src/parser/test.c
+++ src/parser/test.c
@@ -2,9 +2,6 @@
 
 #include "../ast/ast.h"
 #include "../ast/ast_printer.h"
-#include "../common/diagnostics.h"
-
-#include <stdio.h>
 
 int main()
 {
@@ -12,8 +9,7 @@ int main()
 
   if (status == 0)
   {
-    PRINTF_HEADER("Program is " GREEN_LABEL "OK" NORMAL_LABEL ".\nPrinting a reconstruction of your program from AST...");
-    print_full_ast(stdout, ast, 2);
+    print_full_ast(stdout, ast, 0);
   }
 
   return status;
--- src/scanner/test.c
+++ src/scanner/test.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
-#include "../common/diagnostics.h"
-
 extern int yylex();
 
 int main()
 {
-  PRINTF_HEADER("Tokenizing and printing input...");
   while(yylex());
-  printf("\n");
-  PRINTF_HEADER("Program is " GREEN_LABEL "OK" NORMAL_LABEL ".");
   return 0;
 }
